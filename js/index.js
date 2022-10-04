ba5834257563ba3b09d4aa89ae8e3ec25fb15e72
ripatee <risto.korhonen@kapsi.fi>
2022-10-04 22:26:39 +0300

    Add grid size button. Partialy working

--- js/index.js
+++ js/index.js
@@ -1,15 +1,31 @@
-function createDivs(x, y, container) {
+function createDivs(x, y) {
     for(let i = 0; i<x; i++){
         let row = document.createElement("div");
         row.classList.add("row");
+
         for (let j = 0; j<y; j++) {
             let column = document.createElement("div");
             column.classList.add("column");
+            column.addEventListener("mousedown", () => {
+                column.classList.add("hover");
+            })
             row.appendChild(column);
         }
         container.appendChild(row)
     }
 }
+let size = 16;
 
 let container = document.querySelector(".container");
-createDivs(16, 16, container);
+let sizeBtn = document.querySelector(".size");
+
+sizeBtn = addEventListener("click", () => {
+    // Update size of the grid
+    size = prompt("Size of the grid:", 16);
+    while (container.firstChild){
+        container.firstChild.remove();
+    }
+    createDivs(size, size, container);
+});
+
+createDivs(size, size, container);
